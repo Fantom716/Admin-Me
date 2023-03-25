5d6fbe90005045c181bd5badc1c2d081b957d04c
Fantom716 <fantom5431@gmail.com>
2023-03-25 06:06:00 +0400

    Optimization nav

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import BigCard from './components/cards/big card/big-card';
 import SmallCard from './components/cards/small card/small-card';
 import Navigation from './components/nav/navigation.jsx'
 import InformationCard from './components/cards/card for information/informationCard';
+import FormSearch from './components/search form/form';
 
 function App() {
   return (
@@ -10,8 +11,12 @@ function App() {
       <InformationCard />
       <Navigation name="Alexander" />
       <BigCard />
-      <SmallCard />
+      <BigCard />
+      <BigCard />
+
+      {/* <SmallCard />
       <InformationCard titleCard="Менеджеры" name="Шеншин Акентий" mail="tefecreunnitta-4734@yopmail.com"/>
+      <FormSearch /> */}
     </>
   );
 }
---
+++ src/components/search form/form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FormSearch() {
+    return(
+        <form action="post">
+            <input type="text" name="form-search" id="search" />
+        </form>
+    )
+}
+
+export default FormSearch;
