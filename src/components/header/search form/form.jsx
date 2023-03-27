5e2d019d642c2f4687c32fab70c58593c2226181
Fantom716 <fantom5431@gmail.com>
2023-03-27 09:08:52 +0400

    Create navigation form

--- src/components/header/search form/form.jsx
+++ src/components/header/search form/form.jsx
@@ -2,8 +2,14 @@ import React from "react";
 import style from './form.module.css'
 
 function FormSearch() {
-    return(
+    return (
         <form action="post">
+            <div className={style.icon}>
+                <svg width="29" height="29" viewBox="0 0 29 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="13.2917" cy="13.2917" r="7.25" stroke="#222222" />
+                    <path d="M24.1667 24.1667L20.5417 20.5417" stroke="#222222" stroke-linecap="round" />
+                </svg>
+            </div>
             <input type="text" name="form-search" id="search" placeholder="Search" />
         </form>
     )
