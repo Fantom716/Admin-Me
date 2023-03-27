aeeb18ea5cd6eaace918827a2ac2b7252a059b85
Fantom716 <fantom5431@gmail.com>
2023-03-27 07:24:38 +0400

    Create form search

--- src/components/header/assemblyHeader/ready header.jsx
+++ src/components/header/assemblyHeader/ready header.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Greeting from "../greeting/greeting";
 import Notification from "../notification/notification";
 import style from './ready header.module.css'
+import FormSearch from "../search form/form";
 
 function Header() {
     return(
         <header>
             <Greeting />
+            <FormSearch />
             <Notification />
         </header>
     )
--- src/components/header/search form/form.jsx
+++ src/components/header/search form/form.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import style from './form.module.css'
 
 function FormSearch() {
     return(
         <form action="post">
-            <input type="text" name="form-search" id="search" />
+            <input type="text" name="form-search" id="search" placeholder="Search" />
         </form>
     )
 }
